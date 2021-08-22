031c3b60cd421615b643f5ab56defcbccb490fc6
hwgite2016 <admin@portero.com.co>
2021-08-22 11:16:02 -0500

    iconos en footer

--- resources/views/admin/partial/footer.blade.php
+++ resources/views/admin/partial/footer.blade.php
@@ -1,5 +1,40 @@
-<strong>Copyright &copy; 2021 <a href="#">portero.com.co</a>.</strong>
+{{-- <strong>Copyright &copy; 2021 <a href="#">portero.com.co</a>.</strong>
 All rights reserved.
 <div class="float-right d-none d-sm-inline-block">
 <b>Version</b> 1.0.0
-</div>
+</div> --}}
+@can('admin.zonas.index')
+    <a href="{{route('admin.zonas.index')}}" class="btn btn-sm btn-default  float-right mr-2" data-toggle="tooltip" title="Zonas Comunes">
+        <i class="fas fa-swimmer"></i>
+    </a>
+@endcan
+
+@can('admin.seguimiento.index')
+    <a href="{{route('admin.seguimiento.index')}}" class="btn btn-sm btn-default  float-right mr-2" data-toggle="tooltip" title="Correspondencia">
+        <i class="fas fa-inbox"></i>
+    </a>
+@endcan
+
+@can('admin.seguimiento.index')
+    <a href="{{route('admin.seguimiento.index')}}" class="btn btn-sm btn-default  float-right mr-2" data-toggle="tooltip" title="Visitantes">
+        <i class="fas fa-user-clock"></i>
+    </a>
+@endcan
+
+@can('admin.seguimiento.index')
+    <a href="{{route('admin.seguimiento.index')}}" class="btn btn-sm btn-default  float-right mr-2" data-toggle="tooltip" title="Pqr">
+        <i class="fas fa-envelope"></i>
+    </a>
+@endcan
+
+@can('admin.seguimiento.index')
+    <a href="{{route('admin.seguimiento.index')}}" class="btn btn-sm btn-default  float-right mr-2" data-toggle="tooltip" title="Mi Cuenta">
+        <i class="fas fa-user"></i>
+    </a>
+@endcan
+
+@can('admin.seguimiento.index')
+    <a href="{{route('admin.seguimiento.index')}}" class="btn btn-sm btn-default" data-toggle="tooltip" title="Home">
+        <i class="fas fa-home"></i>
+    </a>
+@endcan
